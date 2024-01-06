2408feb3c111642d0dde4014b486521c41201392
SoleMJS <139501506+SoleMJS@users.noreply.github.com>
2024-01-06 22:36:06 +0500

    feat:(src/TodoWrapper&Todo): Реализовал возможность смены состояния выполнения задачи

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,15 @@ import { faPenSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export const Todo = ({ task }) => {
+export const Todo = ({ task, toggleComplete }) => {
 	return (
 		<div className='Todo'>
-			<p>{task.task}</p>
+			<p
+				onClick={() => toggleComplete(task.id)}
+				className={`${task.completed ? 'completed' : ''}`}
+			>
+				{task.task}
+			</p>
 			<div>
 				<FontAwesomeIcon icon={faPenSquare} />
 				<FontAwesomeIcon icon={faTrash} />
