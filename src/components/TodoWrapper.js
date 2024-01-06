a65236da96e0fe123e2672c5d5c372067a9dbfe7
SoleMJS <139501506+SoleMJS@users.noreply.github.com>
2024-01-06 22:51:14 +0500

    feat:(src/TodoWrapper&EditTodoForm): Реализовал возможность редактирования задачи

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { EditTodoForm } from './EditTodoForm'
 import { Todo } from './Todo'
 import { TodoForm } from './TodoForm'
 uuidv4()
@@ -25,18 +26,38 @@ export const TodoWrapper = () => {
 
 	const deleteTodo = id => setTodos(todos.filter(todo => todo.id !== id))
 
+	const editTodo = id =>
+		setTodos(
+			todos.map(todo =>
+				todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+			)
+		)
+
+	const editTask = (task, id) => {
+		setTodos(
+			todos.map(todo =>
+				todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+			)
+		)
+	}
+
 	return (
 		<div className='TodoWrapper'>
 			<h1>Get Things done</h1>
 			<TodoForm addTodo={addTodo} />
-			{todos.map((todo, index) => (
-				<Todo
-					task={todo}
-					key={index}
-					toggleComplete={toggleComplete}
-					deleteTodo={deleteTodo}
-				/>
-			))}
+			{todos.map((todo, index) =>
+				todo.isEditing ? (
+					<EditTodoForm editTodo={editTask} key={index} task={todo} />
+				) : (
+					<Todo
+						task={todo}
+						key={index}
+						toggleComplete={toggleComplete}
+						deleteTodo={deleteTodo}
+						editTodo={editTodo}
+					/>
+				)
+			)}
 		</div>
 	)
 }
